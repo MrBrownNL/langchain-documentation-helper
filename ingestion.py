5518c9c371da820013a2f1051fa1f7cbb19330fd
Jeroen de Bruijn <jeroen.de.bruijn@brightalley.nl>
2024-11-12 19:28:21 +0100

    Added firecrawl

--- ingestion.py
+++ ingestion.py
@@ -32,5 +32,45 @@ def ingest_docs():
     print("*** Loading to vectorstore done ***")
 
 
+def ingest_firecrawl():
+    from langchain_community.document_loaders import FireCrawlLoader
+
+    langchain_documents_base_urls = [
+        "https://python.langchain.com/v0.2/docs/integrations/chat/",
+        "https://python.langchain.com/v0.2/docs/integrations/llms/",
+        "https://python.langchain.com/v0.2/docs/integrations/text_embedding/",
+        "https://python.langchain.com/v0.2/docs/integrations/document_loaders/",
+        "https://python.langchain.com/v0.2/docs/integrations/document_transformers/",
+        "https://python.langchain.com/v0.2/docs/integrations/vectorstores/",
+        "https://python.langchain.com/v0.2/docs/integrations/retrievers/",
+        "https://python.langchain.com/v0.2/docs/integrations/tools/",
+        "https://python.langchain.com/v0.2/docs/integrations/stores/",
+        "https://python.langchain.com/v0.2/docs/integrations/llm_caching/",
+        "https://python.langchain.com/v0.2/docs/integrations/graphs/",
+        "https://python.langchain.com/v0.2/docs/integrations/memory/",
+        "https://python.langchain.com/v0.2/docs/integrations/callbacks/",
+        "https://python.langchain.com/v0.2/docs/integrations/chat_loaders/",
+        "https://python.langchain.com/v0.2/docs/concepts/",
+    ]
+    langchain_documents_base_urls2 = [langchain_documents_base_urls[0]]
+    for url in langchain_documents_base_urls2:
+        print(f"FireCrawling {url=}")
+        loader = FireCrawlLoader(
+            url=url,
+            mode="crawl",
+            params={
+                "limit": 5,
+            },
+        )
+        docs = loader.load()
+
+        print(f"Going to add {len(docs)} documents to Pinecone")
+        PineconeVectorStore.from_documents(
+            docs, embeddings, index_name=os.environ.get("VDB_INDEX_NAME"), namespace=os.environ.get("VDB_FIRECRAWL_NAMESPACE")
+        )
+        print(f"****Loading {url}* to vectorstore done ***")
+
+
 if __name__ == "__main__":
-    ingest_docs()
+    # ingest_docs()
+    ingest_firecrawl()
